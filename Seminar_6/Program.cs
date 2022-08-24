2054d82278654c2556b877f36d10ada452d6da1b
Сергей Игоревич Гаврилов <109974689+NRG367GSI@users.noreply.github.com>
2022-08-24 07:36:37 +0300

    addad home work 5 and seminar 6

---
+++ Seminar_6/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.). 
+Элементы двумерного массива задаются случано и лежат в промежутке от -10 до 10
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+
+Задача 2. Вычислить среднее арифметическое положительных элементов в одномерном массиве. 
+Элементы массива задаются случано и лежат в промежутке от -10 до 10
+[1, -5, 8, 4, -9] -> 4.33
+*/
+int[] array(int size = 10, int min = -10, int max = 11) //Создает массив и заполняет его случайными целочисленными значениями
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+
+    }
+    return array;
+}
+
+void avarage(int size = 10, int min = -10, int max = 11) //среднее арифметическое положительных элементов
+{
+    int[] arr = array(size, min, max);
+    double count = 0;
+    double summ = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > 0)
+        {
+            summ += arr[i];
+            count++;
+        }
+    }
+    double result = summ / count;
+    var str = "[" + String.Join(", ", arr) + "]";
+    Console.WriteLine($"массив{str},сумма: {summ} количество положительных чисел {count} результат: {result:f2}");
+}
+
+void reversArray(int size = 10, int min = -10, int max = 11) //Поворот массива
+{
+int[] arr = array(size, min, max);
+Console.WriteLine("[" + String.Join(", ", arr) + "]");
+for (int i = 0; i < size/2; i++ )
+{
+     int a = arr[i];
+     arr[i] = arr[size-1-i];
+     arr[size-1-i] = a;
+}
+var str = "[" + String.Join(", ", arr) + "]";
+Console.WriteLine(str);
+}
+
+void menu() //Вызывает методы
+{    
+    string paragraph = String.Empty;
+    while (paragraph != "1" || paragraph != "2")
+    {
+        Console.WriteLine("===========================================");
+        Console.WriteLine("Введите 1 для перехода к задаче 1: ");
+        Console.WriteLine("Введите 2 для перехода к задаче 2: ");
+        Console.WriteLine("Введите 0 для выхода из программы: ");
+        Console.WriteLine("===========================================");
+        paragraph = Console.ReadLine();
+        if (paragraph == "1") reversArray();
+        if (paragraph == "2") avarage();
+        if (paragraph == "0") break;
+    }
+}
+
+menu();
---
+++ homework_5/Program.cs
@@ -0,0 +1,88 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных
+чисел в массиве.
+[345, 897, 568, 234] -> 2
+
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+int protections() //принемает значение и преабразует его в целочисленное значени или выдает предупреждение
+{
+    Console.Write("Введите значение: ");
+    if (int.TryParse(Console.ReadLine(), out int number));
+    else number = 999999999;
+    return number;
+}
+
+int[] array(int size = 10, int min = 100, int max = 1000) //Создает массив и заполняет его случайными целочисленными значениями
+{
+    int[] array = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+
+    }
+    return array;
+
+}
+
+int Task_34(int[] array) // Принемает массив и выводит колличество четных элементрв массива
+{
+    int size = array.Length;
+    int even = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int a = array[i];
+        if (a % 2 == 0)
+        {
+            even++;
+        }
+    }
+    return even;
+}
+//Console.WriteLine(Task_34(array()));
+
+void Task_36(int size = 999999999, int min = 999999999, int max = 999999999)
+{
+    while (size == 999999999 || min == 999999999 || max == 999999999 || size <= 0 || min > max)
+    {
+        Console.Write("Введите размер массива: ");
+        size = protections();
+
+        Console.Write("Введите min массива: ");
+        min = protections();
+
+        Console.Write("Введите max массива: ");
+        max = protections();
+        if (size == 999999999 || min == 999999999 || max == 999999999 || size <= 0 || min > max) Console.WriteLine("Вы ввели не корректное значение, повторите ввод: ");
+    }
+    int summ = 0;
+    int[] arr = array(size, min, max);
+    for (int i = 0; i < size; i++)
+    {
+        if ( i % 2 != 0)
+        {
+            summ = summ +arr[i];
+        }
+    }
+    var str = "[" + String.Join(", ", arr) + "]";
+    Console.Write($"{str}, сумма элементов массива с нечетным индексом: {summ}");
+}
+//Task_36();
+
+void Task_38()
+{
+    
+}
+
+
+
+
+
+
