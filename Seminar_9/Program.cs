2b061ef6d0494aea5a27733ca60488af263d866b
Kite343 <109787392+Kite343@users.noreply.github.com>
2022-09-02 23:42:53 +0500

    fixed, StrSplit, NumsArray

--- Seminar_9/Program.cs
+++ Seminar_9/Program.cs
@@ -1,6 +1,5 @@
 ﻿int[] Array(int[] array)
 {
-    int index =
     int size = 0;
     for (int i = 0; i < array.Length; i++)
     {
@@ -11,7 +10,7 @@
     }
     int[] even = new int[size];
     size = 0;
-    for (int j = 0; j < size; j++)
+    for (int j = 0; j < array.Length; j++)
     {
         if (array[j] % 2 == 0)
         {
@@ -28,5 +27,39 @@ void ArrayOtput(int[] array)
     Console.Write($" {array[j]} ");
 }
 
-int[] arr =new int[5] {1, 2, 3 ,4, 5};
-ArrayOtput(Array(arr));
+int[] NumsArray (string[] array)
+{
+    int[] arrayNums = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    arrayNums[i] = int.Parse(array[i]);
+    return arrayNums;
+}
+
+string[] StrSplit (string str)
+{
+    int size = 0;
+    foreach(char c in str)
+    {if(c == char.Parse(",")) size++;}
+    size++;
+
+    string[] numsStr = new string[size];
+    string num = "";
+    size = 0;
+    for(int i = 0; i < str.Length; i++)
+    {
+        if(str[i] == char.Parse(","))
+        {
+            numsStr[size] = num;
+            size++;
+            num = "";
+        } 
+        else
+        num += str[i].ToString();
+    }
+    numsStr[size] = num;
+    return numsStr;
+}
+
+Console.WriteLine("Введите числа через запятую без пробелов");
+ArrayOtput(Array(NumsArray(StrSplit(Console.ReadLine()))));
+Console.WriteLine();
